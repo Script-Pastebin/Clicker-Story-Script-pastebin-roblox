39e2721027dbb3f944ee0b90e968030856f6224b
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:58:26 +0100

    commit zero

---
+++ Clicker Story Script pastebin roblox Clicker Story .lua
@@ -0,0 +1,13 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://pastebin.com/raw/Gqmqf8g5"))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Powerful script and very functional
+Auto farm
+Endless jumps and more :)
+						
+]]
